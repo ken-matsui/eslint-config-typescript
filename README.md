c6d2ce2670a75ace0654fea9acd1daa320acf04d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-15 03:55:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# eslint-config-typescript
+
+## Installation
+
+1. Run the install command
+   ```bash
+   $ npm install -D @ken-matsui/eslint-config-typescript
+   ```
+2. You also need to install peer dependencies
+   ```bash
+   $ npm install -D eslint
+   ```
+
+## Usage
+
+`.eslintrc.yml`
+
+```yaml
+extends:
+  - "@ken-matsui/eslint-config-typescript"
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "import",
+    "@typescript-eslint"
+  ],
+  "extends": [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript"
+  ],
+  "rules": {
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always"
+      }
+    ],
+    "import/no-unresolved": "error",
+    "import/namespace": "error",
+    "import/default": "error",
+    "import/export": "error",
+    "import/no-named-as-default": "error",
+    "import/no-named-as-default-member": "error",
+    "import/no-duplicates": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/member-delimiter-style": "error",
+    "@typescript-eslint/consistent-type-definitions": "error",
+    "@typescript-eslint/explicit-function-return-type": "error"
+  }
+}
